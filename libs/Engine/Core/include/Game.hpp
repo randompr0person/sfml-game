ba6faaa9a9b833e1951400c147d3cfc1c9154661
Anton Muransky <167703645+randompr0person@users.noreply.github.com>
2026-06-23 16:25:27 +0100

    added engine_core library

---
+++ libs/Engine/Core/include/Game.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+namespace Engine::Core {
+
+  class Game {
+
+  public:
+    Game();
+    ~Game();
+
+    void run();
+
+  private:
+
+
+  };
+
+}
---
+++ libs/Engine/Core/src/Game.cpp
@@ -0,0 +1,17 @@
+
+#include "Game.hpp"
+
+Engine::Core::Game::Game()
+{
+
+}
+
+Engine::Core::Game::~Game()
+{
+
+}
+
+void Engine::Core::Game::run()
+{
+
+}
